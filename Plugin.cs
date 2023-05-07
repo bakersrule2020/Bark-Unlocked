a2a2d56c35c46157699f56a09845784c1440230b
ThatRobloxDev <59939720+bakersrule2020@users.noreply.github.com>
2023-05-07 00:03:00 -0400

    Update Plugin.cs

--- Plugin.cs
+++ Plugin.cs
@@ -8,7 +8,6 @@ using Bark.Extensions;
 
 namespace Bark
 {
-    [ModdedGamemode]
     [BepInDependency("org.legoandmars.gorillatag.utilla", "1.5.0")]
     [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
 
